7cd7680b6d3a95a7e1774c1b34be5749234ce1f3
Juhana Soini <juhana.soini@avenis.fi>
2020-01-17 17:17:12 +0200

    Initial

---
+++ app.js
@@ -0,0 +1,40 @@
+/**/
+
+const PORT = process.env.PORT || 8000;
+
+const express = require( 'express' );
+
+const app = express();
+
+app.use( '/path/:tii' , ( req, res, next ) =>{
+	console.log( req.params );
+	res.send( "MOIkka" );
+} );
+app.use( '/path' , ( req, res, next ) =>{
+	console.log( "OKKI" );
+	res.write( "JOU" );
+	next();
+}, ( req, res, next ) => {
+	res.write( "MOI" );
+	// res.send( "MOI" );
+	res.end();
+} );
+
+
+app.use( ( req, res, next ) => {
+	console.log( "PATH "+req.path );
+	next();
+}); 
+
+app.get( '/', ( req, res, next ) => {
+	res.send( "Hello world" );
+} );
+
+app.use( ( req, res, next ) => {
+	res.status( 404 );
+	res.json( { status: 404, message: 'Not found' } );
+} )
+
+app.listen(PORT, () => {
+  console.log(`app running on port ${PORT}`)
+});
